6b5834a7075b76882bdd3e785efa7bc40d583502
Victoriajuszkiewicz <wanderer.foxy@gmail.com>
2023-05-22 18:54:11 +0200

    reviews carousel

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,13 +7,14 @@ import bookish from "../img/bookish.png";
 import weather from "../img/weather.png";
 import petapp from "../img/petapp.png";
 import wellness from "../img/wellness.png";
+import hairdresser from "../img/hair.png";
 
 const Projects = () => {
 	return (
 		<div id="Projects" className="Projectsmain">
 			<div className="container" id="projectstext">
 				<h2>Cool stuf I've built</h2>
-				<p>
+				<p className="webdesigntext">
 					Here's a glimpse of some of the websites I've created with passion and
 					precision.
 					<br />
@@ -26,21 +27,26 @@ const Projects = () => {
 					Allow me to weave my magic and create a digital masterpiece for you.
 				</p>
 			</div>
-			<div className="container justify-content-center">
-				<div className="row justify-content-center">
-					<div className="col-2 col-lg-6  justify-content-center">
+			<div className="container">
+				<div className="row justify-content-center gy-3">
+					<div className="col-2 col-lg-6 d-flex  justify-content-center">
 						<div className="card" style={{ width: "30rem" }}>
 							<img className="card-img-top" src={foody} alt="Card image cap" />
 							<div className="card-body">
 								<h5 className="card-title">Recipe app- FOODY</h5>
-								<a href="#" className="btn btn-primary" id="codebtn">
+								<a
+									href="https://github.com/Victoriajuszkiewicz/FOODY-app-Spoonacular-API-"
+									target="_blank"
+									className="btn btn-primary"
+									id="codebtn"
+								>
 									View code
 								</a>
 							</div>
 						</div>
 					</div>
-					<div className="col-2 col-lg-6  justify-content-center">
-						<div className="card" style={{ width: "30rem" }}>
+					<div className="col-2 col-lg-6  d-flex justify-content-center">
+						<div className="card h-100" style={{ width: "30rem" }}>
 							<img
 								className="card-img-top"
 								src={weather}
@@ -49,17 +55,27 @@ const Projects = () => {
 							/>
 							<div className="card-body">
 								<h5 className="card-title">Weather App</h5>
-								<a href="#" className="btn btn-primary" id="codebtn">
+								<a
+									href="https://github.com/Victoriajuszkiewicz/weather-app-Openweather-API-"
+									target="_blank"
+									className="btn btn-primary"
+									id="codebtn"
+								>
 									View code
 								</a>
-								<a href="#" className="btn btn-primary" id="demobtn">
-									View demo
+								<a
+									href="https://main--weather-app-victoria.netlify.app/"
+									target="_blank"
+									className="btn btn-primary"
+									id="demobtn"
+								>
+									View live
 								</a>
 							</div>
 						</div>
 					</div>
-					<div className="col-2 col-lg-6  justify-content-center">
-						<div className="card" style={{ width: "30rem" }}>
+					<div className="col-2 col-lg-6  d-flex justify-content-center">
+						<div className="card h-100" style={{ width: "30rem" }}>
 							<img
 								className="card-img-top"
 								src={cinemaholics}
@@ -68,14 +84,19 @@ const Projects = () => {
 							/>
 							<div className="card-body">
 								<h5 className="card-title">Netflix clone</h5>
-								<a href="#" className="btn btn-primary" id="codebtn">
+								<a
+									href="https://github.com/Victoriajuszkiewicz/Cinemaholics-TMDB-API-"
+									target="_blank"
+									className="btn btn-primary"
+									id="codebtn"
+								>
 									View code
 								</a>
 							</div>
 						</div>
 					</div>
-					<div className="col-2 col-lg-6  justify-content-center">
-						<div className="card" style={{ width: "30rem" }}>
+					<div className="col-2 col-lg-6  d-flex justify-content-center">
+						<div className="card h-100" style={{ width: "30rem" }}>
 							<img
 								className="card-img-top"
 								src={bookish}
@@ -84,14 +105,19 @@ const Projects = () => {
 							/>
 							<div className="card-body">
 								<h5 className="card-title">Book store</h5>
-								<a href="#" className="btn btn-primary" id="codebtn">
+								<a
+									href="https://github.com/Victoriajuszkiewicz/shopping-cart-js"
+									target="_blank"
+									className="btn btn-primary"
+									id="codebtn"
+								>
 									View code
 								</a>
 							</div>
 						</div>
 					</div>
-					<div className="col-2 col-lg-6  justify-content-center">
-						<div className="card" style={{ width: "30rem" }}>
+					<div className="col-2 col-lg-6 d-flex  justify-content-center">
+						<div className="card h-100" style={{ width: "30rem" }}>
 							<img
 								className="card-img-top"
 								src={petapp}
@@ -100,7 +126,12 @@ const Projects = () => {
 							/>
 							<div className="card-body">
 								<h5 className="card-title">Petapp</h5>
-								<a href="#" className="btn btn-primary" id="codebtn">
+								<a
+									href="https://github.com/Victoriajuszkiewicz/Petapp-Petfinder-API"
+									target="_blank"
+									className="btn btn-primary"
+									id="codebtn"
+								>
 									View code
 								</a>
 							</div>
@@ -108,8 +139,52 @@ const Projects = () => {
 					</div>
 				</div>
 			</div>
-			<div className="container">
-				<h2>Web design</h2>
+			<div className="container" id="webdesigndiv">
+				<h2 className="webdesigntext">Web design</h2>
+				<div className="container">
+					<div className="row justify-content-center gy-3">
+						<div className="col-2 col-lg-6 d-flex  justify-content-center">
+							<div className="card" style={{ width: "30rem" }}>
+								<img
+									className="card-img-top"
+									src={wellness}
+									alt="Card image cap"
+								/>
+								<div className="card-body">
+									<h5 className="card-title">Wellness coach </h5>
+									<a
+										href="https://dribbble.com/shots/21444055-Wellness-coach-website-design"
+										target="_blank"
+										className="btn btn-primary"
+										id="codebtn"
+									>
+										View design
+									</a>
+								</div>
+							</div>
+						</div>
+						<div className="col-2 col-lg-6 d-flex  justify-content-center">
+							<div className="card h-100" style={{ width: "30rem" }}>
+								<img
+									className="card-img-top"
+									src={hairdresser}
+									alt="Card image cap"
+								/>
+								<div className="card-body">
+									<h5 className="card-title">Hairdresser page</h5>
+									<a
+										href="https://dribbble.com/shots/21465230-Hair-salon-hairdresser-website"
+										target="_blank"
+										className="btn btn-primary"
+										id="codebtn"
+									>
+										View design
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -1,18 +1,150 @@
 import React from "react";
 import "./Reviews.css";
 
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
 // Pictures for cards
 import kelly from "../img/kelly.jpeg";
 import Natalia from "../img/Natalia.jpeg";
 import Michelle from "../img/Michelle.jpg";
 
 const Reviews = () => {
+	const responsive = {
+		superLargeDesktop: {
+			// the naming can be any, depends on you.
+			breakpoint: { max: 4000, min: 3000 },
+			items: 5,
+		},
+		desktop: {
+			breakpoint: { max: 3000, min: 900 },
+			items: 3,
+		},
+		tablet: {
+			breakpoint: { max: 900, min: 464 },
+			items: 2,
+		},
+		mobile: {
+			breakpoint: { max: 464, min: 0 },
+			items: 1,
+		},
+	};
+
 	return (
 		<div className="Reviewsmain">
 			<div className="container">
 				<h2 className="titlereviews">What people say about working with me</h2>
 			</div>
 			<div className="container">
+				<Carousel
+					responsive={responsive}
+					infinite={true}
+					autoPlay={true}
+					autoPlaySpeed={2000}
+					keyBoardControl={true}
+				>
+					<div id="cardreview">
+						<div className="card" id="cardreview" style={{ width: "20rem" }}>
+							<img
+								className="card-img-top"
+								src={Natalia}
+								alt="Card image cap"
+								id="reviewimg"
+							/>
+							<div className="card-body">
+								<h5 className="mb-3">Natalia</h5>
+								<h6 className="text-primary mb-3">E-commerce & CRO Manager</h6>
+								<i className="bi bi-star-fill"></i>
+								<p className="px-xl-3">
+									<i className="fas fa-quote-left pe-2"></i>"I had the pleasure
+									to work with Victoria for over a year. She is a very organised
+									person, easy to work with and contributes to the projects in a
+									very unique way.
+									<br />
+									She is a team player, has attention to detail and
+									unquestionable work ethic. <br />
+									Victoria would be a great addition to any team
+									looking for talent."
+								</p>
+							</div>
+						</div>
+					</div>
+					<div>
+						<div className="card h-100" style={{ width: "20rem" }}>
+							<img
+								className="card-img-top"
+								src={Michelle}
+								alt="Card image cap"
+								id="reviewimg"
+							/>
+							<div className="card-body">
+								<h5 className="mb-3">Michelle</h5>
+								<h6 className="text-primary mb-3">CEO</h6>
+								<i className="bi bi-star-fill"></i>
+								<p className="px-xl-3">
+									<i className="fas fa-quote-left pe-2"></i>"My E-commerce brand
+									has worked with Victoria for over a year now and I can
+									honestly say she has been the best addition to my team so far.
+									<br />
+									She works to her own initiative and needs little or not
+									direction as she is super creative.
+									<br />I would highly recommend Victoria."
+								</p>
+							</div>
+						</div>
+					</div>
+					<div>
+						<div className="card h-100" style={{ width: "20rem" }}>
+							<img
+								className="card-img-top"
+								src={Michelle}
+								alt="Card image cap"
+								id="reviewimg"
+							/>
+							<div className="card-body">
+								<h5 className="mb-3">Michelle</h5>
+								<h6 className="text-primary mb-3">CEO</h6>
+								<i className="bi bi-star-fill"></i>
+								<p className="px-xl-3">
+									<i className="fas fa-quote-left pe-2"></i>"My E-commerce brand
+									has worked with Victoria for over a year now and I can
+									honestly say she has been the best addition to my team so far.
+									<br />
+									She works to her own initiative and needs little or not
+									direction as she is super creative.
+									<br />I would highly recommend Victoria."
+								</p>
+							</div>
+						</div>
+					</div>
+					<div>
+						<div className="card h-100" style={{ width: "20rem" }}>
+							<img
+								className="card-img-top"
+								src={Michelle}
+								alt="Card image cap"
+								id="reviewimg"
+							/>
+							<div className="card-body">
+								<h5 className="mb-3">Michelle</h5>
+								<h6 className="text-primary mb-3">CEO</h6>
+								<i className="bi bi-star-fill"></i>
+								<p className="px-xl-3">
+									<i className="fas fa-quote-left pe-2"></i>"My E-commerce brand
+									has worked with Victoria for over a year now and I can
+									honestly say she has been the best addition to my team so far.
+									<br />
+									She works to her own initiative and needs little or not
+									direction as she is super creative.
+									<br />I would highly recommend Victoria."
+								</p>
+							</div>
+						</div>
+					</div>
+				</Carousel>
+			</div>
+
+			{/* <div className="container">
 				<div className="row">
 					<div className="col-sm">
 						<div className="card" style={{ width: "20rem" }}>
@@ -117,7 +249,7 @@ const Reviews = () => {
 						</div>
 					</div>
 				</div>
-			</div>
+			</div> */}
 		</div>
 	);
 };
